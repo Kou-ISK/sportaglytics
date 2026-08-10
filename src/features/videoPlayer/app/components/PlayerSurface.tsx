328a0a3f001f1d1ec68c06160938e5371ddf6676
Kou_ISK <96302956+Kou-ISK@users.noreply.github.com>
2026-08-10 22:11:09 +0900

    refactor(player): separate controller from sync state

--- src/features/videoPlayer/app/components/PlayerSurface.tsx
+++ src/features/videoPlayer/app/components/PlayerSurface.tsx
@@ -170,7 +170,6 @@ export const PlayerSurface: React.FC<PlayerSurfaceProps> = ({
             handleCurrentTime={handleCurrentTime}
             maxSec={maxSec}
             videoList={videoList}
-            syncData={syncData}
             useTimelineClock={useTimelineClock}
           />
         </Box>
